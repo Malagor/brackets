d3266dbc2195f977f3af8335eca62538eb9d80a3
Alex Malagor <seo.dnja@gmail.com>
2020-10-04 22:04:47 +0300

    refactor: loops and funcs

--- src/index.js
+++ src/index.js
@@ -1,42 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
     let stackBrackets = [];
 
-    function isOpenBrackets(bracket) {
+     const isOpenBrackets = (currentBracket) => {
         for (let i = 0; i < bracketsConfig.length; i++) {
-            if (bracket === bracketsConfig[i][0]) {
+            if (currentBracket === bracketsConfig[i][0]) {
                 if (bracketsConfig[i][0] === bracketsConfig[i][1]) return -1;
 
                 return bracketsConfig[i][1];
             }
         }
         return false;
-    }
+    };
 
-    console.log(str);
+    const getLastElement = (array) => {
+        if (Array.isArray(array))
+            return array[array.length - 1];
+        else
+            throw new Error('Передан не массив');
+    };
 
     if ((str.length % 2)) return false;
 
     str.split('').forEach(item => {
+
         const closeBracket = isOpenBrackets(item);
 
         if (closeBracket === false) {
-            if (stackBrackets[stackBrackets.length - 1] !== item) {
-                return false;
-            }
-            stackBrackets.pop();
+
+            return item === getLastElement(stackBrackets)
+                ? stackBrackets.pop()
+                : false;
 
         } else if (closeBracket === -1) {
 
-            if (stackBrackets[stackBrackets.length - 1] === item) {
-                stackBrackets.pop();
-            } else {
-                stackBrackets.push(item);
-            }
+            item === getLastElement(stackBrackets)
+                ? stackBrackets.pop()
+                : stackBrackets.push(item);
+
         } else {
             stackBrackets.push(closeBracket);
         }
-
-        console.log('stackBrackets', stackBrackets);
     });
 
     return !stackBrackets.length;
